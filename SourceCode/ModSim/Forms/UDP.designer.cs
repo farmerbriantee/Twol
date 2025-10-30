b8c5da42898fc08e4950a76157c9feb4884e1863
Brian Tischler <yesgogit@gmail.com>
2025-10-30 11:42:49 -0600

    format Modsim

--- SourceCode/ModSim/Forms/UDP.designer.cs
+++ SourceCode/ModSim/Forms/UDP.designer.cs
@@ -33,6 +33,95 @@ namespace ModSim
 
         public IPAddress ipCurrent;
 
+        static byte[] PGN_253 = { 128, 129, 126, 253, 8, 0, 0, 0, 0, 0, 0, 0, 0, 12 };
+        int PGN_253_Size = PGN_253.Length - 1;
+
+        //Heart beat hello
+        static byte[] helloFromAutoSteer = { 128, 129, 126, 126, 5, 0, 0, 0, 0, 0, 71 };
+
+        //hello from Modules
+        static byte[] helloFromMachine = { 128, 129, 123, 123, 5, 0, 0, 0, 0, 0, 71 };
+
+        //hello from Modules
+        static byte[] helloFromIMU = { 128, 129, 121, 121, 5, 0, 0, 0, 0, 0, 71 };
+
+        //settings pgn
+        static byte[] PGN_237 = { 0x80, 0x81, 0x7f, 237, 8, 1, 2, 3, 4, 0, 0, 0, 0, 0xCC };
+        int PGN_237_Size = PGN_237.Length - 1;
+
+
+        //Relays
+        byte relay = 0, relayHi = 0, uTurn = 0;
+        byte xte = 0;
+
+        //Switches
+        int remoteSwitch = 1, workSwitch = 1, steerSwitch = 1, switchByte = 0;
+
+        //On Off
+        byte guidanceStatus = 0;
+        byte prevGuidanceStatus = 0;
+        bool guidanceStatusChanged = false;
+
+        //speed sent as *10
+        double gpsSpeed = 0, gpsSpeedMM = 0;
+
+        //steering variables
+        double steerAngleActual = 0;
+        double steerAngleSetPoint = 0; //the desired angle from AgOpen
+
+        //Machine module
+        int hydLift = 0;
+        int tramline = 0;
+
+        int relayLoM = 0;
+        int relayHiM = 0;
+
+        //Spray Controller
+        static byte[] PGN_224 = { 0x80, 0x81, 0x7f, 224, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xCC };
+        int PGN_224_Size = PGN_224.Length - 1;
+        double volumePerMinuteSet = 0;
+        double totalVolume = 0;
+        double sectionPercent = 1;
+
+        static byte[] swapBits = {
+        0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
+        0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
+        0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8,
+        0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
+        0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4,
+        0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
+        0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec,
+        0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
+        0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2,
+        0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
+        0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea,
+        0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
+        0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6,
+        0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
+        0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee,
+        0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
+        0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1,
+        0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
+        0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9,
+        0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
+        0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5,
+        0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
+        0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed,
+        0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
+        0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
+        0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
+        0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
+        0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
+        0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7,
+        0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
+        0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef,
+        0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff,  };
+
+
+
+
+
+
         //initialize udp network
         public void LoadUDPNetwork()
         {
@@ -85,6 +174,7 @@ namespace ModSim
             }
         }
 
+        #region NTRIP Listen
         private void ReceiveDataNTRIPAsync(IAsyncResult asyncResult)
         {
             try
@@ -113,7 +203,7 @@ namespace ModSim
         private void ReceiveFromNTRIP(byte[] data)
         {
             try
-            { 
+            {
                 {
                     rtbNtrip.Clear();
                     rtbNtrip.AppendText(BitConverter.ToString(data));
@@ -125,6 +215,7 @@ namespace ModSim
             }
         }
 
+        #endregion
 
         #region Send UDP
 
@@ -187,81 +278,6 @@ namespace ModSim
 
         #region Receive UDP
 
-        private void ReceiveDataUDPAsync(IAsyncResult asyncResult)
-        {
-            try
-            {
-                // Receive all data
-                int msgLen = UDPSocket.EndReceiveFrom(asyncResult, ref endPointUDP);
-
-                byte[] localMsg = new byte[msgLen];
-                Array.Copy(buffer, localMsg, msgLen);
-
-                // Listen for more connections again...
-                UDPSocket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endPointUDP,
-                    new AsyncCallback(ReceiveDataUDPAsync), null);
-
-                BeginInvoke((MethodInvoker)(() => ReceiveFromUDP(localMsg)));
-
-            }
-            catch (Exception)
-            {
-                //WriteErrorLog("UDP Recv data " + e.ToString());
-                //MessageBox.Show("ReceiveData Error: " + e.Message, "UDP Server", MessageBoxButtons.OK,
-                //MessageBoxIcon.Error);
-            }
-        }
-
-        static byte [] PGN_253 = { 128, 129, 126, 253, 8, 0, 0, 0, 0, 0, 0, 0, 0, 12 };
-        int PGN_253_Size = PGN_253.Length - 1;
-
-        //Heart beat hello
-        static byte [] helloFromAutoSteer = { 128, 129, 126, 126, 5, 0, 0, 0, 0, 0, 71 };
-
-        //hello from Modules
-        static byte[] helloFromMachine = { 128, 129, 123, 123, 5, 0, 0, 0, 0, 0, 71 };
-
-        //hello from Modules
-        static byte[] helloFromIMU = { 128, 129, 121, 121, 5, 0, 0, 0, 0, 0, 71 };
-
-        //settings pgn
-        static byte[] PGN_237 = { 0x80, 0x81, 0x7f, 237, 8, 1, 2, 3, 4, 0, 0, 0, 0, 0xCC };
-        int PGN_237_Size = PGN_237.Length - 1;
-
-
-        //Relays
-        byte relay = 0, relayHi = 0, uTurn = 0;
-        byte xte = 0;
-
-        //Switches
-        int remoteSwitch = 1, workSwitch = 1, steerSwitch = 1, switchByte = 0;
-
-        //On Off
-        byte guidanceStatus = 0;
-        byte prevGuidanceStatus = 0;
-        bool guidanceStatusChanged = false;
-
-        //speed sent as *10
-        double gpsSpeed = 0, gpsSpeedMM = 0;
-
-        //steering variables
-        double steerAngleActual = 0;
-        double steerAngleSetPoint = 0; //the desired angle from AgOpen
-
-        //Machine module
-        int hydLift = 0;
-        int tramline = 0;
-
-        int relayLoM = 0;
-        int relayHiM = 0;
-
-        //Spray Controller
-        static byte[] PGN_224 = { 0x80, 0x81, 0x7f, 224, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xCC };
-        int PGN_224_Size = PGN_224.Length - 1;
-        double volumePerMinuteSet=0;
-        double totalVolume = 0;
-        double sectionPercent = 1;
-
         private void ReceiveFromUDP(byte[] data)
         {
             try
@@ -273,12 +289,12 @@ namespace ModSim
                         case 227:
                             {
                                 volumePerMinuteSet = ((double)(data[7] | data[8] << 8)) * 0.01;
-                                lblSetGPM.Text = (Math.Round(volumePerMinuteSet,2)).ToString();
+                                lblSetGPM.Text = (Math.Round(volumePerMinuteSet, 2)).ToString();
 
                                 sectionPercent = (double)(data[9]);
                                 lblBypassPercent.Text = (sectionPercent).ToString();
                                 sectionPercent *= 0.01;
-                                
+
                                 //send back 224
                                 totalVolume += ((volumePerMinuteSet / 300) * sectionPercent);
                                 lblTotalVolume.Text = totalVolume.ToString("N1");
@@ -297,7 +313,7 @@ namespace ModSim
                                 //pressure
                                 PGN_224[9] = unchecked((byte)((int)(52)));
 
-                                
+
                                 //isFlowing
                                 PGN_224[10] = unchecked((byte)((int)(1) >> 8));
 
@@ -540,7 +556,7 @@ namespace ModSim
                                 //make really sure this is the reply pgn
                                 if (data[4] == 3 && data[5] == 202 && data[6] == 202)
                                 {
-                                    byte [] scanReply = { 128, 129, 126, 203, 7,
+                                    byte[] scanReply = { 128, 129, 126, 203, 7,
                                         Properties.Settings.Default.etIP_SubnetOne,
                                         Properties.Settings.Default.etIP_SubnetTwo,
                                         Properties.Settings.Default.etIP_SubnetThree, 126,
@@ -560,7 +576,7 @@ namespace ModSim
                                     {
                                         CK_A = (CK_A + scanReply[i]);
                                     }
-                                    scanReply[scanReply.Length - 1] = unchecked((byte)((int)(CK_A))); 
+                                    scanReply[scanReply.Length - 1] = unchecked((byte)((int)(CK_A)));
 
                                     SendUDPMessage(scanReply);
 
@@ -596,12 +612,12 @@ namespace ModSim
                                 break;
                             }
 
-                            ///////////// Machine Module
+                        ///////////// Machine Module
 
                         case 239:  //machine data
                             {
                                 uTurn = data[5];
-                                lblUTurn.Text=uTurn.ToString();
+                                lblUTurn.Text = uTurn.ToString();
 
                                 gpsSpeedMM = (double)data[6];//actual speed times 4, single uint8_t
                                 gpsSpeedMM *= 0.1;
@@ -667,8 +683,9 @@ namespace ModSim
 
                                 //set1 
                                 int sett = data[8];  //setting0     
-                                if ((sett & (1 << 0)) != 0) 
-                                    aogConfig.isRelayActiveHigh = 1; else aogConfig.isRelayActiveHigh = 0;
+                                if ((sett & (1 << 0)) != 0)
+                                    aogConfig.isRelayActiveHigh = 1;
+                                else aogConfig.isRelayActiveHigh = 0;
                                 lblRelayActiveHigh.Text = aogConfig.isRelayActiveHigh.ToString();
 
                                 aogConfig.user1 = data[9];
@@ -705,44 +722,33 @@ namespace ModSim
             }
         }
 
+        private void ReceiveDataUDPAsync(IAsyncResult asyncResult)
+        {
+            try
+            {
+                // Receive all data
+                int msgLen = UDPSocket.EndReceiveFrom(asyncResult, ref endPointUDP);
+
+                byte[] localMsg = new byte[msgLen];
+                Array.Copy(buffer, localMsg, msgLen);
+
+                // Listen for more connections again...
+                UDPSocket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endPointUDP,
+                    new AsyncCallback(ReceiveDataUDPAsync), null);
+
+                BeginInvoke((MethodInvoker)(() => ReceiveFromUDP(localMsg)));
+
+            }
+            catch (Exception)
+            {
+                //WriteErrorLog("UDP Recv data " + e.ToString());
+                //MessageBox.Show("ReceiveData Error: " + e.Message, "UDP Server", MessageBoxButtons.OK,
+                //MessageBoxIcon.Error);
+            }
+        }
+
         #endregion
-
-        static byte [] swapBits = {
-        0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
-        0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
-        0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8,
-        0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
-        0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4,
-        0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
-        0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec,
-        0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
-        0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2,
-        0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
-        0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea,
-        0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
-        0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6,
-        0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
-        0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee,
-        0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
-        0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1,
-        0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
-        0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9,
-        0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
-        0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5,
-        0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
-        0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed,
-        0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
-        0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
-        0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
-        0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
-        0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
-        0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7,
-        0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
-        0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef,
-        0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff,  };
-
     }
-
 
 
     public static class steerConfig
