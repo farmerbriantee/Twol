fbd23c7ffe1128a50e2597cdc63e1ea1b2cf17ce
p00qwerty Gamer <p00qwerty@outlook.com>
2026-03-14 07:55:10 +0100

    Aero Snap + Aero Peek

---
+++ SourceCode/Twol/Forms/OverlayForm.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Twol
+{
+    partial class OverlayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // OverlayForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(374, 231);
+            this.ControlBox = false;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "OverlayForm";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Snapsize Overlay";
+            this.TopMost = true;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OverlayForm_Paint);
+            this.Resize += new System.EventHandler(this.OverlayForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
